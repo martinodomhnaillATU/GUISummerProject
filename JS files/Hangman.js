3b59865288c5320d9292bb1b16f942ad1283a0f9
RioLuis <G00484614@atu.ie>
2026-04-03 21:47:25 +0100

    edited

--- JS files/Hangman.js
+++ JS files/Hangman.js
@@ -8,68 +8,97 @@ const words = [
   "melody","harmony","rhythm","guitarist","pianist","violinist","percussion","symphony","orchestra","composer",
   "waterfall","mountain","volcano","rainforest","desert","island","valley","glacier","canyon","plateau",
   "education","knowledge","assignment","notebook","curriculum","lecture","homework","examination","research","scholar"];
-  let n1,n,i,word1,length = "";
+  let n1,n,i,word1,letters = "",sum=0,maximumScore=0;
 function rounds()
 {
     n = document.getElementById("n").value;
     
-    n1 = Math.floor(Math.random()*100);
-    word1 = words[n1];
-    length += word1.charAt(0)+" ";
     
-    document.getElementById("RoundNo").innerHTML = "Round 1";
-    for(let j = 1; j <= word1.length - 1; j++)
-    {
-        length += "_ ";
-    }
-    document.getElementById("word").innerHTML = length;
-    document.getElementById("placeholder").innerHTML = "";
-
-}
-
-    
-    function hangman()
+    for(let i = 1 ; j <= n ; i++)
     {
         
-        let word2 = document.getElementById("guess").value.toLowerCase();
-        if(word1 === word2)
+        n1 = Math.floor(Math.random()*100);
+        word1 = words[n1];
+        letters += word1.charAt(0)+" ";
+        maximumScore += word1.length*5;
+        document.getElementById("RoundNo").innerHTML = "Round "+i;
+        for(let j = 1; j <= word1.length - 1; j++)
         {
-            document.getElementById("edit").innerHTML="Well done you guessed it correctly.Refresh the web page to play again";
+            letters += "_ ";
         }
-        else
-        {
+        document.getElementById("word").innerHTML = letters;
+        document.getElementById("placeholder").innerHTML = "";
+        document.getElementById("img").innerHTML = "<img src='hangmanStart.jpg' width='200px' height='200px'>"
 
-            i++;
-            if(i == 6)
-                document.getElementById("edit").innerHTML = "You lose. Game Over. The number was "+n1;
-            else if(n1 > n2)
-                document.getElementById("edit").innerHTML = "Too low. Try again";
-            else
-                document.getElementById("edit").innerHTML = "Too high. Try again";
-            switch(i)
+        let j = 1;
+        while(j <= 6)
+        {
+            function hangman()
             {
-                case 1:
-                document.getElementById("img").innerHTML = "<img src='hangman1.jpg' width='200px' height='200px'>";
-                break;
-                case 2:
-                document.getElementById("img").innerHTML = "<img src='hangman2.jpg' width='200px' height='200px'>";
-                break;
-                case 3:
-                document.getElementById("img").innerHTML = "<img src='hangman3.jpg' width='200px' height='200px'>";
-                break;
-                case 4:
-                document.getElementById("img").innerHTML = "<img src='hangman4.jpg' width='200px' height='200px'>";
-                break;
-                case 5:
-                document.getElementById("img").innerHTML = "<img src='hangman5.jpg' width='200px' height='200px'>";
-                break;
-                case 6:
-                document.getElementById("img").innerHTML = "<img src='hangman6.jpg' width='200px' height='200px'>";
-                break;
+                
+                let word2 = document.getElementById("guess").value.toLowerCase();
+                if(word1 === word2)
+                {
+                    document.getElementById("edit").innerHTML="Well done! You guessed it correctly!";
+                    sum = maximumScore;
+                    j = 6;
 
+                }
+                else
+                {
+
+                    j++;
+                    letters = word1.charAt(0)+" ";
+                    for(let k = 1;  k < word1.length; k++)
+                    {
+                        
+                        
+                            if(word2.contains(word1.charAt(k)))
+                            {
+
+                                sum += 5;
+                                letters += word1.charAt(k);
+                            }
+                            else
+                                letters += "_ ";
+                        
+                    }
+                    document.getElementById("word").innerHTML = letters;
+                    switch(j)
+                    {
+                        case 1:
+                        document.getElementById("img").innerHTML = "<img src='hangman1.jpg' width='200px' height='200px'>";
+                        break;
+                        case 2:
+                        document.getElementById("img").innerHTML = "<img src='hangman2.jpg' width='200px' height='200px'>";
+                        break;
+                        case 3:
+                        document.getElementById("img").innerHTML = "<img src='hangman3.jpg' width='200px' height='200px'>";
+                        break;
+                        case 4:
+                        document.getElementById("img").innerHTML = "<img src='hangman4.jpg' width='200px' height='200px'>";
+                        break;
+                        case 5:
+                        document.getElementById("img").innerHTML = "<img src='hangman5.jpg' width='200px' height='200px'>";
+                        break;
+                        case 6:
+                        document.getElementById("img").innerHTML = "<img src='hangman6.jpg' width='200px' height='200px'>";
+                        break;
+
+                    }
+                }
             }
+
+
+
+
         }
-
-
-
+        localStorage.gamesPlayed = n;
+        localStorage.pointsObtained = sum;
+        localStorage.maxPoints = maximumScore;
+        document.getElementById("button").innerHTML = "<a href='Hangman3.html'><button id='b2'>Display Results</button></a>";
+        document.getElementById("b2").style.color = "blue";
+        document.getElementById("b2").style.backgroundColor = "white";
+        document.getElementById("b2").style.textAlign = "center";
     }
+}
