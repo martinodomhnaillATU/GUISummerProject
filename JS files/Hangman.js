d1b204859219c689fadaaaee4312b3b0ced9d53c
RioLuis <G00484614@atu.ie>
2026-04-03 12:26:55 +0100

    edited

--- JS files/Hangman.js
+++ JS files/Hangman.js
@@ -1,10 +1,38 @@
-let i = 0;
-    let n1 = Math.floor(Math.random()*100)+1;
+const words = [
+  "apple","banana","orange","grape","cherry","mango","peach","apricot","coconut","papaya",
+  "vehicle","bicycle","scooter","subway","rocket","airplane","helicopter","tramway","sailboat","spaceship",
+  "furniture","cabinet","bookshelf","drawer","mirror","carpet","cushion","wardrobe","nightstand","tabletop",
+  "elephant","giraffe","kangaroo","alligator","chimpanzee","dolphin","penguin","rhinoceros","salamander","porcupine",
+  "crimson","turquoise","magenta","indigo","emerald","violet","scarlet","charcoal","lavender","maroon",
+  "joyful","melancholy","furious","delighted","anxious","confident","fearless","graceful","curious","restless",
+  "melody","harmony","rhythm","guitarist","pianist","violinist","percussion","symphony","orchestra","composer",
+  "waterfall","mountain","volcano","rainforest","desert","island","valley","glacier","canyon","plateau",
+  "education","knowledge","assignment","notebook","curriculum","lecture","homework","examination","research","scholar"];
+  let n1,n,i,word1,length = "";
+function rounds()
+{
+    n = document.getElementById("n").value;
+    
+    n1 = Math.floor(Math.random()*100);
+    word1 = words[n1];
+    length += word1.charAt(0)+" ";
+    
+    document.getElementById("RoundNo").innerHTML = "Round 1";
+    for(let j = 1; j <= word1.length - 1; j++)
+    {
+        length += "_ ";
+    }
+    document.getElementById("word").innerHTML = length;
+    document.getElementById("placeholder").innerHTML = "";
+
+}
+
+    
     function hangman()
     {
         
-        let n2 = Number(document.getElementById("val").value);
-        if(n1 == n2)
+        let word2 = document.getElementById("guess").value.toLowerCase();
+        if(word1 === word2)
         {
             document.getElementById("edit").innerHTML="Well done you guessed it correctly.Refresh the web page to play again";
         }
